b22bcdababa62b3f0336c98721f42570062881f3
Aymen Mosrati <aymenmosrati6@gmail.com>
2023-02-07 00:41:42 +0100

    login component

--- src/pages/Auth/Login/index.js
+++ src/pages/Auth/Login/index.js
@@ -1,6 +1,8 @@
+import ReCAPTCHA from "react-google-recaptcha";
 import "./_index.scss";
 import Input from "../../../components/Input";
 import Label from "../../../components/Label";
+import Button from "../../../components/Button";
 import Logo from "../../../assets/img/IconComponent/Logo";
 const Login = () => {
   return (
@@ -9,25 +11,34 @@ const Login = () => {
         <Logo className="login-logo" />
       </div>
       <form className="login-form">
-        <h3 className="login-form-title">Giriş Yap</h3>
+        <h3 className="login-form-title">Login</h3>
         <span className="login-form-description">
-          Sisteme kayıtla e-posta adresinizle giriş yapabilirsiniz.
+          You can log in to the system with your e-mail address.
         </span>
         <div className="login-form-email">
-          <Label label="E-posta Adresi" />
+          <Label label="E-mail address" />
           <Input type="email" className="input" />
         </div>
         <div className="login-form-password">
-          <Label label="Şifre" />
+          <Label label="Password" />
           <Input type="password" className="input" />
         </div>
         <div>
           <Input type="checkbox" className="" id="check" />
-          <Label label="Beni Hatırla" for="check" />
-          <span> Şifremi Unuttum</span>
+          <Label label="Remember me" for="check" />
+          <span> I forgot my password</span>
         </div>
-
-        <button></button>
+        <ReCAPTCHA sitekey="6Lc4m1skAAAAAPWWeYgkqt89o3OEkCJ7dio74T7c" />
+        <Button text="Login" className="button-login" />
+        <p className="login-form-description-help">
+          If you encounter a problem while logging into the system, please
+          contact
+          <span className="login-form-description-help-email">
+            {" "}
+            Yemekoperasyonekibi@migrosonline.com
+          </span>
+          .
+        </p>
       </form>
     </div>
   );
